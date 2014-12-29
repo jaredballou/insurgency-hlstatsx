88dba3ebc0885cdcd231c6c849babb8c816d1b2d
Jared Ballou <github@jballou.com>
2014-12-28 19:42:42 -0500

    Updated latest images and script changes

--- heatmaps/create-hlstats-icons.php
+++ heatmaps/create-hlstats-icons.php
@@ -5,7 +5,7 @@ mysql_connect(DB_HOST,DB_USER,DB_PASS);
 mysql_select_db(DB_NAME);
 
 //Source for weapon images
-$srcpath = 'src/materials/vgui/inventory';
+$srcpath = 'src/insurgency-data/materials/vgui/inventory';
 //Destination for processed results
 $dstpath = '../web/hlstatsimg/games/insurgency';
 //TODO: Merge this all together
@@ -45,14 +45,20 @@ $gaward = 'gawards_nohand.png';
 //Get weapons from MySQL
 $result = mysql_query("SELECT * FROM hlstats_Weapons WHERE game='insurgency' ORDER BY code ASC");
 //Create directories if needed
+/*
 foreach ($directories as $dir => $data) {
 	$path = "{$dstpath}/{$dir}";
 	if (!is_dir($path)) {
 		mkdir($path,0755,true);
 	}
 }
+*/
 //Display list of commands to create images
 while ($row = mysql_fetch_array($result)) {
+//	var_dump($row['code'],getvgui($row['code']));
+//exit;
+	$srcimg = getvgui($row['code']);
+	echo "Processing {$row['code']} ({$row['name']})\n";
 	$shortname = explode('_',$row['code'],2);
 	$shortname = $shortname[1];
 
@@ -60,19 +66,42 @@ while ($row = mysql_fetch_array($result)) {
 
 	//Weapon icon
 	$resize = "-gravity center -trim +repage -background none -resize {$directories['weapons']['res']} -extent {$directories['weapons']['res']}";
-	exec("convert {$srcpath}/{$row['code']}.png {$resize} {$caption} {$dstpath}/weapons/{$row['code']}.png");
+	doexec("convert {$srcimg} {$resize} {$caption} {$dstpath}/weapons/{$row['code']}.png");
 	$caption = '';
 	//Daily award
 	$resize = "-gravity center -trim +repage -background none -resize {$directories['dawards']['res']} -extent {$directories['dawards']['res']}";
-	exec("convert src/{$daward} {$srcpath}/{$row['code']}.png {$resize} {$caption} -compose over -composite {$dstpath}/dawards/w_{$row['code']}.png");
+	doexec("convert src/{$daward} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/dawards/w_{$row['code']}.png");
 	//Global award
 	$resize = "-gravity center -trim +repage -background none -resize {$directories['gawards']['res']} -extent {$directories['gawards']['res']}";
-	exec("convert src/{$gaward} {$srcpath}/{$row['code']}.png {$resize} {$caption} -compose over -composite {$dstpath}/gawards/w_{$row['code']}.png");
+	doexec("convert src/{$gaward} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/gawards/w_{$row['code']}.png");
 	//Ribbons
 	$resize = "-gravity center -trim +repage -background none -resize {$directories['ribbons']['res']} -extent {$directories['ribbons']['res']}";
-	foreach ($ribbons as $prefix => $srcimg) {
-		exec("convert src/{$srcimg} {$srcpath}/{$row['code']}.png {$resize} {$caption} -compose over -composite {$dstpath}/ribbons/{$prefix}{$shortname}.png");
+	foreach ($ribbons as $prefix => $ribimg) {
+		doexec("convert src/{$ribimg} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/ribbons/{$prefix}{$shortname}.png");
 	}
 }
+function doexec($cmd) {
+//	echo "DEBUG: Running {$cmd}\n";
+	exec($cmd);
+}
+function getvgui($name,$path='materials/vgui/inventory') {
+	$rootpath = 'src/insurgency-data';
+	$rp = "{$rootpath}/{$path}/{$name}";
+//var_dump($rp);
+	if (file_exists("{$rp}.vmt")) {
+//echo "found file<br>";
+		$vmf = file_get_contents("{$rp}.vmt");
+//var_dump($vmf);
+		preg_match_all('/basetexture[" ]+([^"\s]*)/',$vmf,$matches);
+//var_dump($matches);
+		$rp = "{$rootpath}/materials/".$matches[1][0];
+	}
+
+//var_dump($rp);
+	if (file_exists("{$rp}.png")) {
+		return "{$rp}.png";
+	}
+}
+
 exit;
 ?>
