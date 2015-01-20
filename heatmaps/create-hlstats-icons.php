ad2f2380af53f8949617d4154e3eb6e2f18e0185
Jared Ballou <github@jballou.com>
2015-01-20 01:57:37 -0500

    Updated image generation script and added map icons for Insurgency

--- heatmaps/create-hlstats-icons.php
+++ heatmaps/create-hlstats-icons.php
@@ -5,26 +5,22 @@ mysql_connect(DB_HOST,DB_USER,DB_PASS);
 mysql_select_db(DB_NAME);
 
 //Source for weapon images
-$srcpath = 'src/insurgency-data/materials/vgui/inventory';
+$srcpath = 'src/insurgency-data/materials';
 //Destination for processed results
 $dstpath = '../web/hlstatsimg/games/insurgency';
 //TODO: Merge this all together
 //Array of directories and the size of the icon
 
-/*
-$p = new PharData("${dstpath}/images.tar");
-$p->buildFromDirectory($dstpath,'/^[^im]/');
-$p2 = $p->compress(Phar::BZ2);
-exit;
-*/
 $directories = array(
 	'dawards' => array(
 		'res' => '112x64',
-		'caption' => true
+		'caption' => true,
+		'background' => 'src/dawards_nohand.png'
 	),
 	'gawards' => array(
 		'res' => '112x64',
-		'caption' => false
+		'caption' => false,
+		'background' => 'src/gawards_nohand.png'
 	),
 	'maps' => array(
 		'res' => '174x174',
@@ -41,12 +37,11 @@ $directories = array(
 );
 //Array of ribbons
 $ribbons = array(
-	'rb_' => 'bronze_ribbons_nohand.png',
-	'rs_' => 'silver_ribbons_nohand.png',
-	'rg_' => 'gold_ribbons_nohand.png'
+	'rb_' => 'src/bronze_ribbons_nohand.png',
+	'rs_' => 'src/silver_ribbons_nohand.png',
+	'rg_' => 'src/gold_ribbons_nohand.png'
 );
 //Daily Award background
-$daward = 'dawards_nohand.png';
 //Global award background
 $gaward = 'gawards_nohand.png';
 //Get weapons from MySQL
@@ -60,11 +55,11 @@ foreach ($directories as $dir => $data) {
 	}
 }
 */
-//Display list of commands to create images
+//Create weapon images
 while ($row = mysql_fetch_array($result)) {
 //	var_dump($row['code'],getvgui($row['code']));
 //exit;
-	$srcimg = getvgui($row['code']);
+	$srcimg = getvgui($row['code'],'vgui/inventory');
 	echo "Processing {$row['code']} ({$row['name']})\n";
 	$shortname = explode('_',$row['code'],2);
 	$shortname = $shortname[1];
@@ -77,37 +72,73 @@ while ($row = mysql_fetch_array($result)) {
 	$caption = '';
 	//Daily award
 	$resize = "-gravity center -trim +repage -background none -resize {$directories['dawards']['res']} -extent {$directories['dawards']['res']}";
-	doexec("convert src/{$daward} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/dawards/w_{$row['code']}.png");
+	doexec("convert {$directories['dawards']['background']} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/dawards/w_{$row['code']}.png");
 	//Global award
 	$resize = "-gravity center -trim +repage -background none -resize {$directories['gawards']['res']} -extent {$directories['gawards']['res']}";
-	doexec("convert src/{$gaward} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/gawards/w_{$row['code']}.png");
+	doexec("convert {$directories['gawards']['background']} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/gawards/w_{$row['code']}.png");
 	//Ribbons
 	$resize = "-gravity center -trim +repage -background none -resize {$directories['ribbons']['res']} -extent {$directories['ribbons']['res']}";
 	foreach ($ribbons as $prefix => $ribimg) {
-		doexec("convert src/{$ribimg} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/ribbons/{$prefix}{$shortname}.png");
+		doexec("convert {$ribimg} {$srcimg} {$resize} {$caption} -compose over -composite {$dstpath}/ribbons/{$prefix}{$shortname}.png");
 	}
 }
+
+//Create map images
+$mappath = "{$srcpath}/vgui/endroundlobby/maps";
+$mapfiles = glob("{$mappath}/*.*");
+$maps = array();
+foreach ($mapfiles as $map) {
+	$basename = remove_ext(basename($map));
+	$noext = remove_ext($map);
+	if (isset($maps[$basename]))
+		continue;
+	if (file_exists("{$noext}.png")) {
+		$maps[$basename] = "{$noext}.png";
+		continue;
+	}
+	$srcimg = getvgui($basename,'vgui/endroundlobby/maps');
+	if ($srcimg)
+		$maps[$basename] = $srcimg;
+}
+
+foreach ($maps as $mapname => $mapimg) {	
+	echo "Processing {$mapname}\n";
+	$resize = "-gravity center -trim +repage -background none -resize {$directories['maps']['res']} -extent {$directories['maps']['res']}";
+	doexec("convert {$mapimg} {$resize} {$dstpath}/maps/{$mapname}.png");
+}
+
+//Create archive of images
+$dirs = implode('|',array_keys($directories));
+$arc = "${dstpath}/images.zip";
+if (file_exists($arc))
+	unlink($arc);
+$p = new PharData($arc);
+$p->buildFromDirectory($dstpath,"/\/({$dirs})\//");
+exit;
+
+
+function remove_ext($str) {
+	$noext = preg_replace('/(.+)\..*$/', '$1', $str);
+//	print "input: $str\n";
+//	print "output: $noext\n\n";
+	return $noext;
+}
+
 function doexec($cmd) {
-//	echo "DEBUG: Running {$cmd}\n";
+	echo "DEBUG: Running {$cmd}\n";
 	exec($cmd);
 }
-function getvgui($name,$path='materials/vgui/inventory') {
-	$rootpath = 'src/insurgency-data';
-	$rp = "{$rootpath}/{$path}/{$name}";
-//var_dump($rp);
+function getvgui($name,$path='vgui/inventory') {
+	$rp = "{$GLOBALS['srcpath']}/{$path}/{$name}";
 	if (file_exists("{$rp}.vmt")) {
-//echo "found file<br>";
 		$vmf = file_get_contents("{$rp}.vmt");
-//var_dump($vmf);
 		preg_match_all('/basetexture[" ]+([^"\s]*)/',$vmf,$matches);
-//var_dump($matches);
-		$rp = "{$rootpath}/materials/".$matches[1][0];
+		$rp = "{$GLOBALS['srcpath']}/".$matches[1][0];
 	}
-
-//var_dump($rp);
 	if (file_exists("{$rp}.png")) {
 		return "{$rp}.png";
 	}
+	return NULL;
 }
 
 exit;
